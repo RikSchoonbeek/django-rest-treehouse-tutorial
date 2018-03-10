c2a363709cabb1f1156c6e2f6633ffbd5d5d0d38
RikSchoonbeek <rik.schoonbeek@gmail.com>
2018-03-10 14:48:14 +0100

    Full CRUD - generic views (2.3)

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,14 @@ from . import views
 
 app_name = 'courses'
 urlpatterns = [
-    path('', views.ListCreateTodoList.as_view(), name="list_todolists"),
+    path('', views.ListCreateTodoList.as_view(), name="list_list"),
+    path('<int:pk>/',
+         views.RetrieveUpdateDestroyTodoList.as_view(),
+         name='list_detail'),
+    path('<int:list_pk>/items/',
+         views.ListCreateTodoListItem.as_view(),
+         name="item_list"),
+    path('<int:list_pk>/items/<int:item_pk>/',
+         views.RetrieveUpdateDestroyTodoListItem.as_view(),
+         name="item_detail")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,21 +1,49 @@
-from rest_framework import status  # For sending back status codes
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 from . import models
 from . import serializers
 
 
-class ListCreateTodoList(APIView):
-    def get(self, request, format=None): # format controls the format that comes back out
-        todolists = models.TodoList.objects.all()
-        serializer = serializers.TodoListSerializer(todolists, many=True)
-        # many=True because we are serializing multiple objects
-        return Response(serializer.data)
+class ListCreateTodoList(generics.ListCreateAPIView):
+    queryset = models.TodoList.objects.all()
+    serializer_class = serializers.TodoListSerializer
 
-    def post(self, request, format=None):
-        serializer = serializers.TodoListSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        # If data is not valid, view stops and exception is raised
-        serializer.save()  # Saves instance to database, but also updates serializer with 'id' field
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class RetrieveUpdateDestroyTodoList(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.TodoList.objects.all()
+    serializer_class = serializers.TodoListSerializer
+
+
+class ListCreateTodoListItem(generics.ListCreateAPIView):
+    queryset = models.TodoListItem.objects.all()
+    serializer_class = serializers.TodoListItemSerializer
+
+    def get_queryset(self):
+        """
+        Determine correct queryset in order to get only items for the specific list.
+        Without this, the method will return all items
+        """
+        return self.queryset.filter(todolist_id=self.kwargs.get('list_pk'))
+
+    def perform_create(self, serializer):
+        """
+        Prevent user from giving a different pk when they create a new item
+        perform_create is run when an object is being created by the view
+        """
+        todolist = get_object_or_404(
+            models.TodoList, pk=self.kwargs.get('list_pk')
+        )
+        serializer.save(todolist=todolist)
+
+
+class RetrieveUpdateDestroyTodoListItem(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.TodoListItem.objects.all()
+    serializer_class = serializers.TodoListItemSerializer
+
+    def get_object(self):
+        return get_object_or_404(
+            self.get_queryset(),
+            todolist_id=self.kwargs.get('list_pk'),
+            pk=self.kwargs.get('pk')
+        )
